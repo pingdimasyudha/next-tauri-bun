c0b5a0acd5ec9eb81c362a3c9ad6457064bee02d
Dimas Yudha Pratama <pingdimasyudha@icloud.com>
2024-12-30 21:08:12 +0700

    feat: initialize a client side project with `Tailwind CSS`, `Next.js`, & `Bun`

---
+++ next.config.ts
@@ -0,0 +1,14 @@
+import type { NextConfig } from "next";
+
+type OutputType = "standalone" | "export";
+
+const nextConfig: NextConfig = {
+  output: (process.env.NEXT_OUTPUT as OutputType) ?? "standalone",
+  distDir: "build",
+  images: {
+    unoptimized: true,
+  },
+  assetPrefix: process.env.NEXT_HOST ?? "http://localhost:3000",
+};
+
+export default nextConfig;
